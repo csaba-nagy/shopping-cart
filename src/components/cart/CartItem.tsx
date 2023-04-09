e4b72e1c2b2cf371322f33e7162a47b328467017
Csaba Nagy <n.csaba1991@gmail.com>
2023-04-09 21:56:39 +0000

    refactor: outsource functions

--- src/components/cart/CartItem.tsx
+++ src/components/cart/CartItem.tsx
@@ -1,9 +1,21 @@
+import { useCallback } from 'react'
 import { useAppDispatch } from '../../app/hooks'
 import { changeAmount, removeItemFromCart } from '../../features/cart/cart.slice'
 import type { CartItemType } from '../../features/cart/types'
 
 function CartItem({ id, title, price, amount, thumbnail }: CartItemType) {
   const dispatch = useAppDispatch()
+
+  const onClickDecrement = useCallback(() => {
+    if (amount === 1)
+      return dispatch(removeItemFromCart({ id }))
+
+    dispatch(changeAmount({ id, method: 'decrease' }))
+  }, [amount, dispatch, id])
+
+  const onClickIncrement = useCallback(() => {
+    dispatch(changeAmount({ id, method: 'increase' }))
+  }, [dispatch, id])
 
   return (
     <article className="cart-item">
@@ -18,15 +30,13 @@ function CartItem({ id, title, price, amount, thumbnail }: CartItemType) {
         <button
           type="button"
           className="amount-button increase"
-          onClick={() => dispatch(changeAmount({ id, method: 'increase' }))}
+          onClick={ onClickIncrement }
           >+</button>
         <p className="cart-item-amount">{ amount }</p>
         <button
           type="button"
           className="amount-button decrease"
-          onClick={() => dispatch(amount === 1
-            ? removeItemFromCart({ id })
-            : changeAmount({ id, method: 'decrease' }))}
+          onClick={ onClickDecrement }
           >-</button>
       </div>
       </article>
